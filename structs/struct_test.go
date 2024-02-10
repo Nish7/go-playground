23a0f204e214e3777185f7f0003acf75196b0a05
Nishil <nishilkapadia17@gmail.com>
2024-02-10 14:50:06 -0500

    chore: added structs pkg

---
+++ structs/struct_test.go
@@ -0,0 +1,37 @@
+package structs
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := rectangle.Perimeter()
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f wanted %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{"Rectangle", Rectangle{Width: 12, Height: 6}, 72.0},
+		{"Circle", Circle{Radius: 10}, 14},
+		{"Triangle", Triangle{Base: 10, Height: 10}, 50},
+	}
+
+	for _, tt := range areaTests {
+
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+
+			if got != tt.want {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+			}
+		})
+
+	}
+}
