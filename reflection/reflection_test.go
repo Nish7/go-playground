131233795fb1b6263066eee298e0171922a305f6
Nishil <nishilkapadia17@gmail.com>
2024-02-15 23:55:32 -0500

    chore: started with reflection pkg

---
+++ reflection/reflection_test.go
@@ -0,0 +1,22 @@
+package reflection
+
+import (
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+	expected := "Chris"
+	var got []string
+
+	x := struct {
+		Name string
+	}{expected}
+
+	walk(x, func(input string) {
+		got = append(got, input)
+	})
+
+	if got[0] != expected {
+		t.Errorf("wrong number of functions calls, got %q want %q", got, 1)
+	}
+}
