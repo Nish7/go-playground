b06d539d01b56c3b5d830de5c857d3f84e4b92e9
Nishil <nishilkapadia17@gmail.com>
2024-02-16 00:31:13 -0500

    chore: reflection unfinished, moving on

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -1,22 +1,73 @@
 package reflection
 
 import (
+	"reflect"
 	"testing"
 )
 
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
 func TestWalk(t *testing.T) {
-	expected := "Chris"
-	var got []string
 
-	x := struct {
-		Name string
-	}{expected}
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
 
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
-
-	if got[0] != expected {
-		t.Errorf("wrong number of functions calls, got %q want %q", got, 1)
+			"strict	with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Nishil", 12},
+			[]string{"Nishil"},
+		},
+		{
+			"nested structs",
+			Person{
+				"Nsihil",
+				Profile{33, "London"},
+			},
+			[]string{"Nishi", "asdas"},
+		},
+		{
+			"pointers to thing",
+			&Person{
+				"chirs",
+				Profile{33, "Nishil"},
+			},
+			[]string{"Nihil"},
+		},
 	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+
 }
