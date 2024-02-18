cf3acedcc46f8a566b4c6df8ab37447c11b5f1cc
Nishil <nishilkapadia17@gmail.com>
2024-02-18 02:28:02 -0500

    chore: update context pkg

--- context/context_test.go
+++ context/context_test.go
@@ -11,6 +11,7 @@ import (
 type StubStore struct {
 	response  string
 	cancelled bool
+	t         *testing.T
 }
 
 func (s *StubStore) Fetch() string {
@@ -18,25 +19,73 @@ func (s *StubStore) Fetch() string {
 	return s.response
 }
 
+func (s *StubStore) assertWasCancelled() {
+	s.t.Helper()
+	if !s.cancelled {
+		s.t.Error("store was not told to cancelled")
+	}
+}
+
+func (s *StubStore) assertWasNotCancelled() {
+	s.t.Helper()
+
+	if s.cancelled {
+		s.t.Error("store was told to cancelled")
+	}
+}
+
 func (s *StubStore) Cancel() {
 	s.cancelled = true
 }
 
 func TestServer(t *testing.T) {
-	data := "hello, world"
-	svr := Server(&StubStore{data, false})
+	t.Run("return data from the store", func(t *testing.T) {
+		data := "hello, world"
+		store := &StubStore{response: data, t: t}
+		svr := Server(store)
 
-	request := httptest.NewRequest(http.MethodGet, "/", nil) // mocks the request object to be sent to mock server
+		request := httptest.NewRequest(http.MethodGet, "/", nil) // mocks the request object to be sent to mock server
 
-	cancellingCtx, cancel = context.WithCancel(request.Context())
-	time.AfterFunc(5*time.Millisecond, cancel)
+		response := httptest.NewRecorder() //  mocks an response writer interface, so it can capture when handler writes to it, allowing us to inspect anad verify
+		// this way of testing is dependecy injection
 
-	response := httptest.NewRecorder() //  mocks an response writer interface, so it can capture when handler writes to it, allowing us to inspect anad verify
-	// this way of testing is dependecy injection
+		svr.ServeHTTP(response, request)
 
-	svr.ServeHTTP(response, request)
+		if response.Body.String() != data {
+			t.Errorf("got %s, wanted %s", response.Body.String(), data)
+		}
 
-	if response.Body.String() != data {
-		t.Errorf("got %s, wanted %s", response.Body.String(), data)
-	}
+		store.assertWasCancelled()
+	})
+
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+
+		data := "hello, world"
+		store := &StubStore{response: data, t: t}
+		svr := Server(store)
+
+		// mocks the request object to be sent to mock server
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		// with cancel return a new context and cancel func, which invoked does finish the
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+
+		request = request.WithContext(cancellingCtx)
+
+		// this runs ayncrnous on a go routine
+		time.AfterFunc(5*time.Millisecond, cancel)
+
+		response := httptest.NewRecorder()
+		//  mocks an response writer interface, so it can capture when handler writes to it, allowing
+		// us to inspect and verify
+		// this way of testing is dependecy injection
+
+		svr.ServeHTTP(response, request)
+
+		if response.Body.String() != data {
+			t.Errorf("got %s, wanted %s", response.Body.String(), data)
+		}
+
+		store.assertWasCancelled()
+	})
 }
