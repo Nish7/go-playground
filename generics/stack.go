5bf92699f74e2df3f603152370f01ba698adfeed
Nishil <nishilkapadia17@gmail.com>
2024-03-25 01:57:06 -0400

    feat: add generics package

---
+++ generics/stack.go
@@ -0,0 +1,30 @@
+package generics
+
+type Stack[T any] struct {
+	values []T
+}
+
+func (s *Stack[T]) Push(value T) {
+	s.values = append(s.values, value)
+}
+
+func (s *Stack[T]) IsEmpty() bool {
+	return len(s.values) == 0
+}
+
+func (s *Stack[T]) Pop() (T, bool) {
+	if s.IsEmpty() {
+		var zero T
+		return zero, false
+	}
+
+	// get the item
+	// and store the item
+	// remove from negavitve index
+	// return the item
+
+	index := len(s.values) - 1
+	val := s.values[index]
+	s.values = s.values[:index]
+	return val, true
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,10 @@ module hello_world
 go 1.21.6
 
 require github.com/approvals/go-approval-tests v0.0.0-20220530063708-32d5677069bd
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
